c9bdb8236d8712874b4a803cb2a58c904379fc0e
N9A <gnianngnua@gmail.com>
2016-01-31 23:08:20 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -78,4 +78,4 @@ colnames(finalData) <- gsub(")","",colnames(finalData))
 ## Question #5: From the data set in step 4, creates a second, 
 ## independent tidy data set with the average of each variable for each activity and each subject.
 TidyDataSet <- ddply(finalData, c("subjectID","activityDescription"), numcolwise(mean))
-write.table(TidyDataSet, file = "TidyDataSet.txt")
+write.table(TidyDataSet, file = "TidyDataSet.txt", row.name=FALSE)
